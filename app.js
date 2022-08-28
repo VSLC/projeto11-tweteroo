804de59e752f9ebda5788b7461c49df0ee524dce
Voller <voller.silas@gmail.com>
2022-08-28 17:47:46 -0300

    feat: Adicionando o retorno da requisição post na rota /sign-up

--- app.js
+++ app.js
@@ -3,11 +3,40 @@ import cors from "cors"
 
 const port = 5000;
 const server = express();
-server.use(cors())
+let tweets = [];
+let users = [];
+let backUp = {
+    username: "",
+    avatar: "",
+}
 
-server.get("/", (req, res) => {
-    res.send("Iniciando projeto")
-});
+server.use(cors())
+server.use(express.json())
+
+server.post("/sign-up", (req, res) => {
+    const user = req.body;
+    backUp.username = user.username;
+    backUp.avatar = user.avatar
+    users.push(user);
+    res.send("ok");
+})
+
+server.post("/tweets", (req, res) => {
+    const tweet = req.body;
+    tweets.push({ ...backUp, ...tweet });
+    res.send("ok");
+
+
+})
+
+server.get("/tweets", (req, res) => {
+    if (tweets.length <= 10) {
+        res.send(tweets)
+    } else {
+        const tweetsSlice = tweets.slice(tweets.length - 10, tweets.length)
+        res.send(tweetsSlice);
+    }
+})
 
 server.listen(port, () => {
     console.log(`Listen on port : ${port}`)
